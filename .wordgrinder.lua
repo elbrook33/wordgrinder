d183efe6b901bf6fbfb89b15c9d0db5ea7042967
Michael Dickison <michaeldickison@gmail.com>
2015-06-12 10:16:36 +1000

    My config

---
+++ .wordgrinder.lua
@@ -0,0 +1,14 @@
+X11_FONT = "FreeMono"
+X11_BLACK_COLOUR = "#ffffff"
+X11_DIM_COLOUR = "#bbbbbb"
+-- X11_NORMAL_COLOUR = "#444444"
+-- X11_BRIGHT_COLOUR = "#222222"
+X11_NORMAL_COLOUR = "#000000"
+X11_BRIGHT_COLOUR = "#000000"
+DocumentSet.statusbar = false
+-- OverrideKey("^T", "SO")
+-- OverrideKey("^O", "FO")
+-- OverrideKey("^E", "FO")
+-- OverrideKey("^L", "SP6")
+-- OverrideKey("^H", "SP1")
+-- OverrideKey("^T", "SP0")
